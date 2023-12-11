8c7d003cde5425a341bae7cd2402bbeeb152c575
rayhan9802 <mdrayhaanahmed@gmail.com>
2023-12-11 10:45:39 +0600

    share my skills what i know

--- src/Pages/Home/Skills/Skills.jsx
+++ src/Pages/Home/Skills/Skills.jsx
@@ -2,6 +2,10 @@ import img1 from '../../../assets/icons/js.png'
 import img2 from '../../../assets/icons/react.png'
 import img3 from '../../../assets/icons/mongo.png'
 import img4 from '../../../assets/icons/nodejs.png'
+import img5 from '../../../assets/icons/figma.jpg'
+import img6 from '../../../assets/icons/css.png'
+import img7 from '../../../assets/icons/html logo.png'
+import img8 from '../../../assets/icons/firebase.jpg'
 
 const Skills = () => {
     return (
@@ -33,6 +37,30 @@ const Skills = () => {
                     <img src={img3} alt="" />
                     </div>
                     <h3 className="text-xl font-bold my-3">MongoDB</h3>
+                </div>
+                <div className="p-7 shadow-xl text-center">
+                    <div className='flex justify-center'>
+                    <img className='w-20' src={img5} alt="" />
+                    </div>
+                    <h3 className="text-xl font-bold my-3">Figma</h3>
+                </div>
+                <div className="p-7 shadow-xl text-center">
+                    <div className='flex justify-center'>
+                    <img className='w-16' src={img6} alt="" />
+                    </div>
+                    <h3 className="text-xl font-bold my-3">CSS</h3>
+                </div>
+                <div className="p-7 shadow-xl text-center">
+                    <div className='flex justify-center'>
+                    <img className='w-16' src={img7} alt="" />
+                    </div>
+                    <h3 className="text-xl font-bold my-3">HTML</h3>
+                </div>
+                <div className="p-7 shadow-xl text-center">
+                    <div className='flex justify-center'>
+                    <img className='w-20' src={img8} alt="" />
+                    </div>
+                    <h3 className="text-xl font-bold my-3">Firebase</h3>
                 </div>
             </div>
         </div>
